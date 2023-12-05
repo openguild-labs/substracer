9eac384ff807a1b581837c6c52a0be4d638be647
tin-snowflake <cqtin0903@gmail.com>
2023-12-05 13:03:42 +0700

    impl node typed builder

--- server/src/controllers/node_controller.rs
+++ server/src/controllers/node_controller.rs
@@ -1,14 +1,25 @@
-use crate::models::node_mdl::NodeModel;
+use crate::models::node_mdl::{NodeModel, TypedNodeIdentityBuilder};
 use axum::{http::StatusCode, Json};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
 pub struct AddNewNode {
-    node_name: String,
+    address: String,
+    node_name: Option<String>,
+    dns: Option<String>,
 }
 
 // add_new_node: Add new node to the simulated network
 pub async fn add_new_node(Json(payload): Json<AddNewNode>) -> Result<Json<NodeModel>, StatusCode> {
-    let node_result = NodeModel::new(payload.node_name);
+    let mut node_builder = TypedNodeIdentityBuilder::default().address(payload.address);
+    if payload.node_name != None {
+        node_builder = node_builder.name(payload.node_name.unwrap());
+    }
+
+    if payload.dns != None {
+        node_builder = node_builder.dns(payload.dns.unwrap());
+    }
+
+    let node_result = node_builder.build();
     Ok(Json(node_result))
 }
--- server/src/models/node_mdl.rs
+++ server/src/models/node_mdl.rs
@@ -1,12 +1,66 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Default, Serialize, Debug, Eq, PartialEq, PartialOrd, Ord)]
-pub struct NodeModel {
-    node_name: String,
+pub struct HasAddress {
+    address: String,
 }
 
-impl NodeModel {
-    pub fn new(_name: String) -> Self {
-        NodeModel { node_name: _name }
+pub struct NoAddress;
+
+pub struct TypedNodeIdentityBuilder<Address> {
+    name: Option<String>,
+    dns: Option<String>,
+    address: Address,
+}
+
+impl Default for TypedNodeIdentityBuilder<NoAddress> {
+    fn default() -> Self {
+        Self {
+            address: NoAddress,
+            name: None,
+            dns: None,
+        }
     }
+}
+
+impl<NoAddress> TypedNodeIdentityBuilder<NoAddress> {
+    pub fn address(self, addr: String) -> TypedNodeIdentityBuilder<HasAddress> {
+        TypedNodeIdentityBuilder {
+            address: HasAddress { address: addr },
+            dns: self.dns,
+            name: self.name,
+        }
+    }
+}
+
+impl TypedNodeIdentityBuilder<HasAddress> {
+    pub fn name(self, name: String) -> TypedNodeIdentityBuilder<HasAddress> {
+        Self {
+            name: Some(name),
+            address: self.address,
+            dns: self.dns,
+        }
+    }
+
+    pub fn dns(self, dns: String) -> TypedNodeIdentityBuilder<HasAddress> {
+        Self {
+            address: self.address,
+            dns: Some(dns),
+            name: self.name,
+        }
+    }
+
+    pub fn build(self) -> NodeModel {
+        NodeModel {
+            address: self.address.address,
+            node_name: self.name,
+            dns: self.dns,
+        }
+    }
+}
+
+#[derive(Default, Serialize, Deserialize, Debug, Eq, PartialEq, PartialOrd, Ord)]
+pub struct NodeModel {
+    address: String,
+    node_name: Option<String>,
+    dns: Option<String>,
 }
